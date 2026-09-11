f0f8043aeb3e0bad70dc20b288d95da96e5b6b77
akshbpatel35 <akshbpatel35@gmail.com>
2026-09-11 16:11:25 +0530

    Initial cab management app

---
+++ cab_management/cab_management/doctype/vehicle/vehicle.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2026, Akshay Patel and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Vehicle(Document):
+	def before_save(self):
+		self.set_title()
+		
+
+	def set_title(self):
+		self.title = f"{self.make} {self.model} , {self.year}"
+		
