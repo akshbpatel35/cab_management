796490c734f4f62ff5b0f6e6b719c0e715677672
akshbpatel35 <akshbpatel35@gmail.com>
2026-09-12 16:37:05 +0530

    added new feature to the app

--- cab_management/cab_management/doctype/ride_booking/ride_booking.js
+++ cab_management/cab_management/doctype/ride_booking/ride_booking.js
@@ -1,8 +1,37 @@
 // Copyright (c) 2026, Akshay Patel and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ride Booking", {
-// 	refresh(frm) {
+frappe.ui.form.on("Ride Booking", {
+    refresh(frm) {
+    },
 
-// 	},
-// });
+    rate(frm) {
+        frm.trigger("udpate_total_amount")
+    },
+
+    udpate_total_amount(frm) {
+        let total_d = 0;
+        for (let item of frm.doc.items) {
+            total_d += item.distance;
+        }
+
+        const amount = frm.doc.rate * total_d;
+        frm.set_value("total_amount", amount)
+    },
+
+});
+
+frappe.ui.form.on('Ride Booking Item', {
+    refresh(frm) {
+        // your code here
+    },
+
+    distance(frm, cdt, cdn) {
+        frm.trigger("udpate_total_amount");
+    },
+    items_remove(frm) {
+        console.log("function called");
+
+        frm.trigger("udpate_total_amount");
+    }
+})
