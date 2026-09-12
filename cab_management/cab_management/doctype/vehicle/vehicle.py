c7a188a060b8a3c27a0f85ef9dabd3e059139ee5
akshbpatel35 <akshbpatel35@gmail.com>
2026-09-12 16:54:56 +0530

    remove white space from the file

--- cab_management/cab_management/doctype/vehicle/vehicle.py
+++ cab_management/cab_management/doctype/vehicle/vehicle.py
@@ -8,8 +8,8 @@ from frappe.website.website_generator import WebsiteGenerator
 class Vehicle(WebsiteGenerator):
 	def before_save(self):
 		self.set_title()
-		
+
 
 	def set_title(self):
 		self.title = f"{self.make} {self.model} , {self.year}"
-		
+
